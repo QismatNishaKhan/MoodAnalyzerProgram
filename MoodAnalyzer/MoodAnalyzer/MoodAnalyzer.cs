9846be16e619c643fbff4ba74fecd0968cbb4731
qismat <qismatnisha6243@gmail.com>
2023-02-22 19:04:05 +0530

    [qismat] Add :UC1-AbilityToAnalyseHappyOrSad

---
+++ MoodAnalyzer/MoodAnalyzer/MoodAnalyzer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerP
+{
+    public class MoodAnalyzer
+    {
+        public string message;
+        
+        public MoodAnalyzer()
+        {
+        }
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyseMood(string message)
+        {
+            if (message.ToUpper().Contains("SAD"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
---
+++ MoodAnalyzer/MoodAnalyzer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodAnalyzerP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
